b866bc0dd340cf6765adf289edebf9e1baf167ef
Dwayne Keane <dkcreativestudios.jm@gmail.com>
2022-06-03 17:36:01 -0500

    Final

--- events.js
+++ events.js
@@ -1,15 +1,51 @@
 const EventEmitter = require ('events');
-const myEmitter = EventEmitter();
+const http = require ('http');
+
+
+class Sales extends EventEmitter{
+    constructor() {
+        super();
+    }
+}
+const myEmitter = new Sales();
+
+
 
 myEmitter.on('newSale', ()=>{
     console.log('There was a new sale!');
 
 })
+
 myEmitter.on('newSale', ()=>{
     console.log('Customer name: John Doe');
 
 })
 
+myEmitter.on('newSale', stock =>{
+    console.log(`There are ${stock}  items left in stock.`);
+})
+
+myEmitter.emit('newSale', 9);
 
 
-myEmitter.emit('newSale');
+///////////////////////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request received!");
+  console.log(req.url);
+  res.end("Request received");
+});
+
+server.on("request", (req, res) => {
+  console.log("Another request 😀");
+});
+
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...");
+});
